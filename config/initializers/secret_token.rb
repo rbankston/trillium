3bbe18b61af40a0eb25f654cd68b7794fcbac47b
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-02-21 00:34:10 -0500

    New Website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trillium::Application.config.secret_token = '63d9d65220facb22076bdfc80bf02613af935eb88838fb3a01a450218db4c983f2961253aa0b89bd54001ff5c0d3f6490fab06812c84fb2bbc2c59082c404e57'
